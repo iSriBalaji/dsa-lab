6083b963bd8d59328ca10d5835906df2f339d8e8
isribalaji <srisvit@gmail.com>
2025-12-10 17:31:00 -0500

    init commit

---
+++ linked_list.py
@@ -0,0 +1,20 @@
+# linked list
+import sys
+print("Linked List")
+
+a = 3
+b = "how arew you doing"
+
+print(a)
+print(b)
+
+a = 7187829182
+b = 345
+c = True
+
+print(sys.getsizeof(a))
+print(sys.getsizeof(b))
+print(sys.getsizeof(c))
+
+
+
---
+++ trees.py
@@ -0,0 +1,5 @@
+print('test')
+
+a = 10
+
+print(id(a))
