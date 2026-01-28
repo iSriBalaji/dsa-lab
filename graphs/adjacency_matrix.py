721b0320fab63736b32733650bac8dad23a9c0db
OctoSeeker <octoseeker@thinkpad>
2026-01-27 23:49:29 -0500

    update notes on graphs

---
+++ graphs/adjacency_matrix.py
@@ -0,0 +1,31 @@
+# example of the adjacency matric to implement in the logic below
+#      A  B  C  D
+#    +------------
+#  A | 0  1  1  1
+#  B | 1  0  1  0
+#  C | 1  1  0  0
+#  D | 1  0  0  0
+# 
+# here there is no direction are weight, we just have the data which vertex is connected to which vertex
+
+vertexes = ['A','B','C','D']
+
+adjacency_matrix = [
+    [0,1,1,1],
+    [1,0,1,0],
+    [1,1,0,0],
+    [1,0,0,0]
+]
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+def print_connections(matrix, vertexes):
+    for i in vertexes:
+        print(i)
+
+print(vertexes)
+print_matrix(adjacency_matrix)
+
+print("")
---
+++ graphs/dijkstra_algorithm.py
@@ -0,0 +1,6 @@
+# to find the shortest part from one node to the other, mostly in weighted graph
+# Reference video: best: https://www.youtube.com/watch?v=EFg3u_E6eHU
+# Code implementation reference video: https://www.youtube.com/watch?v=bZkzH5x0SKU
+
+
+
